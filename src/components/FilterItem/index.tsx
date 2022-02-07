0dc6e180e985e2bfd0c27b620fbd4fe1d52d92ca
joaolucas2012 <ribeirojoaolucas68@gmail.com>
2022-02-07 16:12:39 -0300

    ✨ feat: add filter item

--- src/components/FilterItem/index.tsx
+++ src/components/FilterItem/index.tsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import { useDataValue } from "src/context/JourneysContext";
 import api from "src/services/api";
 
-import { Container } from "./styles";
+import { Container, FilterItems, Name, Number, SvgIcon } from "./styles";
 
 interface IFilterItem {
   id: string;
@@ -12,7 +12,7 @@ interface IFilterItem {
 }
 
 const FilterItem = ({ id, quantity, name }: IFilterItem) => {
-  const [{ journey }, dispatch] = useDataValue();
+  const [dispatch] = useDataValue();
   const [active, setActive] = useState(false);
   const [check, setCheck] = useState(false);
 
@@ -47,17 +47,16 @@ const FilterItem = ({ id, quantity, name }: IFilterItem) => {
         onChange={handleChange}
         onClick={() => chosenJourney(id)}
         data-active={active}
-        journey={journey}
         defaultChecked={check}
       />
       <label htmlFor={id}>
-        <span className={`filterItens ${active}`} id={id}>
-          <p className={`svg icon${id}`} />
+        <FilterItems id={id} active={active}>
+          <SvgIcon id={id} />
 
-          <p className="name">{name}</p>
+          <Name active={active}>{name}</Name>
 
-          <p className="number"> {quantity}</p>
-        </span>
+          <Number active={active}> {quantity}</Number>
+        </FilterItems>
       </label>
     </Container>
   );
--- src/components/FilterItem/styles.ts
+++ src/components/FilterItem/styles.ts
@@ -1,3 +1,76 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const Container = styled.div``;
+import icon5 from "../../assets/icons/bed.svg";
+import icon4 from "../../assets/icons/check.svg";
+import icon1 from "../../assets/icons/paper-plane.svg";
+import icon3 from "../../assets/icons/pen.svg";
+import icon2 from "../../assets/icons/play-circle.svg";
+import icon0 from "../../assets/icons/table.svg";
+
+const icons = [icon0, icon1, icon2, icon3, icon4, icon5];
+
+export const Container = styled.div`
+  input[type="checkbox"] {
+    position: absolute;
+    z-index: -1;
+    opacity: 0;
+  }
+`;
+
+export const FilterItems = styled.span<{
+  active?: boolean;
+}>`
+  display: flex;
+  align-items: center;
+  margin: 20px 0;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export const SvgIcon = styled.p<{
+  id?: string;
+}>`
+  ${({ id }) => css`
+    min-width: 23px;
+    height: 23px;
+    margin-right: 11px;
+    z-index: 2;
+
+    background-image: url(${icons[id]});
+  `}
+`;
+
+export const Name = styled.p<{
+  active?: boolean;
+}>`
+  ${({ active, theme }) => css`
+    color: ${active ? theme.colors.colorBlock : theme.colors.colorTextName};
+    font-weight: ${active ? "bold" : 400};
+    width: 121px;
+
+    &:hover {
+      color: ${({ theme }) => theme.colors.colorBlock};
+      font-weight: ${active ? "bold" : 400};
+    }
+  `}
+`;
+
+export const Number = styled.p<{
+  active?: boolean;
+}>`
+  ${({ active, theme }) => css`
+    color: #9196ab;
+    font-size: 13px;
+    text-align: center;
+    height: 23px;
+    width: 23px;
+    border-radius: 50%;
+    padding: 3.5px;
+
+    background: ${active ? theme.colors.colorBlock : "#E4E6F1"};
+    color: ${active ? theme.colors.colorBg : theme.colors.colorTextName};
+    font-weight: ${active ? "bold" : 400};
+  `}
+`;
